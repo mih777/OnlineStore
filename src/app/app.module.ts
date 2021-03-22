2fbfa5ed23d4214dded54da963a8b6c1d600a1cf
mih777 <mih777777@yandex.ru>
2021-03-22 17:45:32 +0300

    menu is almost ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './comps/header/header.component';
+import { ContentComponent } from './comps/content/content.component';
+import { FooterComponent } from './comps/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
